d6457de78d229832a31762bb3b19ee6d7b3c484a
Shyamala Umamaheswaran <shykart2203@gmail.com>
2016-11-20 19:33:21 +0100

    Extract score calculation into method object

--- src/main/java/com/ennovate/bowling/Frame.java
+++ src/main/java/com/ennovate/bowling/Frame.java
@@ -7,6 +7,7 @@ class Frame {
     private ArrayList<Integer> rolls = new ArrayList<>();
     private int score;
     private Frame nextFrame;
+    private int frameIndex;
 
     Frame roll(int pins) {
         rolls.add(pins);
@@ -14,62 +15,23 @@ class Frame {
     }
 
     int score() {
-        score = totalPinsKnocked();
-        if (isStrike()) {
-            score += strikeBonus();
-        } else if (isSpare()) {
-            score += spareBonus();
-        }
-
-        return score;
+        return new FrameScoreCalculator(this).calculate();
     }
 
     private int totalPinsKnocked() {
         return rolls.stream().mapToInt(Integer::intValue).sum();
     }
 
-    private boolean isSpare() {
-        return !isStrike() && (score == 10);
-    }
-
-    private int spareBonus() {
-        return firstRoll(nextFrame);
-    }
-
     private boolean isStrike() {
-        return (rolls.get(0) == 10) && !isTenthFrame();
-    }
-
-    private int strikeBonus() {
-
-        if (consecutiveStrikes()) {
-            return 10 + firstRoll(nextFrame.getNextFrame());
-        }
-        else if (isNinthFrame())
-        {
-            return nextFrame.getRolls().stream()
-                    .limit(2)
-                    .mapToInt(Integer::intValue)
-                    .sum();
-        } else {
-            return nextFrame.totalPinsKnocked();
-        }
+        return (rolls.get(0) == 10) && !(frameIndex == 10);
     }
 
     private boolean consecutiveStrikes() {
-        return isStrike() && nextFrame.isStrike() && !isNinthFrame();
+        return isStrike() && nextFrame.isStrike() && !(frameIndex == 9);
     }
 
-    private int firstRoll(Frame frame) {
+    private int pinsKnockedInFirstRoll(Frame frame) {
         return frame.getRolls().get(0);
-    }
-
-    private boolean isNinthFrame() {
-        return nextFrame.getNextFrame() == null;
-    }
-
-    private boolean isTenthFrame() {
-        return nextFrame == null;
     }
 
 
@@ -83,5 +45,53 @@ class Frame {
 
     private ArrayList<Integer> getRolls() {
         return rolls;
+    }
+
+    public void setFrameIndex(int frameIndex) {
+        this.frameIndex = frameIndex;
+    }
+
+    protected class FrameScoreCalculator {
+
+        private Frame frame;
+
+        public FrameScoreCalculator(Frame frame) {
+            this.frame = frame;
+        }
+
+        public int calculate() {
+            score = totalPinsKnocked();
+            if (isStrike()) {
+                score += strikeBonus();
+            } else if (isSpare()) {
+                score += spareBonus();
+            }
+
+            return score;
+        }
+
+        private int strikeBonus() {
+
+            if (consecutiveStrikes()) {
+                return 10 + pinsKnockedInFirstRoll(nextFrame.getNextFrame());
+            }
+            else if (frameIndex == 9)
+            {
+                return nextFrame.getRolls().stream()
+                        .limit(2)
+                        .mapToInt(Integer::intValue)
+                        .sum();
+            } else {
+                return nextFrame.totalPinsKnocked();
+            }
+        }
+
+        private boolean isSpare() {
+            return !isStrike() && (score == 10);
+        }
+
+        private int spareBonus() {
+            return pinsKnockedInFirstRoll(nextFrame);
+        }
     }
 }
