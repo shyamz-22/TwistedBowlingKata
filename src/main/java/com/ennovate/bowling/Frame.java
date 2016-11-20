84c3d01fb0bf21bec600ffb2cb09c942eddc9bf1
Shyamala Umamaheswaran <shykart2203@gmail.com>
2016-11-20 22:43:12 +0100

    move methods to ScoreCalculator

--- src/main/java/com/ennovate/bowling/Frame.java
+++ src/main/java/com/ennovate/bowling/Frame.java
@@ -1,13 +1,27 @@
 package com.ennovate.bowling;
 
+import com.ennovate.bowling.score.FrameScoreCalculator;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.ArrayList;
 
-class Frame {
+public class Frame {
 
-    private ArrayList<Integer> rolls = new ArrayList<>();
-    private int score;
+    @Getter
+    private ArrayList<Integer> rolls;
+
+    @Getter
+    @Setter
     private Frame nextFrame;
+
+    @Getter
+    @Setter
     private int frameIndex;
+
+    Frame() {
+        this.rolls = new ArrayList<>();
+    }
 
     Frame roll(int pins) {
         rolls.add(pins);
@@ -15,83 +29,7 @@ class Frame {
     }
 
     int score() {
-        return new FrameScoreCalculator(this).calculate();
+        return new FrameScoreCalculator(this).score();
     }
 
-    private int totalPinsKnocked() {
-        return rolls.stream().mapToInt(Integer::intValue).sum();
-    }
-
-    private boolean isStrike() {
-        return (rolls.get(0) == 10) && !(frameIndex == 10);
-    }
-
-    private boolean consecutiveStrikes() {
-        return isStrike() && nextFrame.isStrike() && !(frameIndex == 9);
-    }
-
-    private int pinsKnockedInFirstRoll(Frame frame) {
-        return frame.getRolls().get(0);
-    }
-
-
-    void setNextFrame(Frame nextFrame) {
-        this.nextFrame = nextFrame;
-    }
-
-    private Frame getNextFrame() {
-        return nextFrame;
-    }
-
-    private ArrayList<Integer> getRolls() {
-        return rolls;
-    }
-
-    public void setFrameIndex(int frameIndex) {
-        this.frameIndex = frameIndex;
-    }
-
-    protected class FrameScoreCalculator {
-
-        private Frame frame;
-
-        public FrameScoreCalculator(Frame frame) {
-            this.frame = frame;
-        }
-
-        public int calculate() {
-            score = totalPinsKnocked();
-            if (isStrike()) {
-                score += strikeBonus();
-            } else if (isSpare()) {
-                score += spareBonus();
-            }
-
-            return score;
-        }
-
-        private int strikeBonus() {
-
-            if (consecutiveStrikes()) {
-                return 10 + pinsKnockedInFirstRoll(nextFrame.getNextFrame());
-            }
-            else if (frameIndex == 9)
-            {
-                return nextFrame.getRolls().stream()
-                        .limit(2)
-                        .mapToInt(Integer::intValue)
-                        .sum();
-            } else {
-                return nextFrame.totalPinsKnocked();
-            }
-        }
-
-        private boolean isSpare() {
-            return !isStrike() && (score == 10);
-        }
-
-        private int spareBonus() {
-            return pinsKnockedInFirstRoll(nextFrame);
-        }
-    }
 }
